3a17987d51d7a07f920f77df3a1fa1d46da7829f
Sylvain Ni <sylvainni@msn.com>
2025-08-25 11:37:40 +0200

    changed to accept empty date, return current date

--- partie-3/sous-programme v0/DATEFMT.cbl
+++ partie-3/sous-programme v0/DATEFMT.cbl
@@ -1,20 +1,28 @@
-000100 IDENTIFICATION DIVISION.
-000200 PROGRAM-ID. DATEFMT.
-000300
-000400********************************************************
-000500 ENVIRONMENT DIVISION.
-000600
-000700********************************************************
-000800 DATA DIVISION.
-000900 WORKING-STORAGE SECTION.
-001000
-001100 LINKAGE SECTION.
-001200 77 LS-DATE-FORMATEE PIC X(10).
-001300
-001400********************************************************
-001500 PROCEDURE DIVISION USING LS-DATE-FORMATEE.
-001600
-001700* Retourne une date simple en format string
-001800     MOVE '08/08/2025' TO LS-DATE-FORMATEE.
-001900
-002000     GOBACK.
+000100  IDENTIFICATION DIVISION. 
+000200  PROGRAM-ID. DATEFMT. 
+000300                                                         
+000400 ********************************************************
+000500  ENVIRONMENT DIVISION. 
+000600                                                         
+000700 ********************************************************
+000800  DATA DIVISION. 
+000900  WORKING-STORAGE SECTION. 
+000910  77 WS-CURR-DATE-21  PIC X(21). 
+001000                                                         
+001100  LINKAGE SECTION. 
+001110  77 LS-DATE-IN       PIC X(8). 
+001200  77 LS-DATE-OUT      PIC X(10). 
+001300                                                         
+001400 ********************************************************
+001500  PROCEDURE DIVISION USING LS-DATE-IN LS-DATE-OUT. 
+001510      MOVE SPACES TO LS-DATE-OUT 
+001600                                                         
+001610      IF LS-DATE-IN = SPACES OR LS-DATE-IN = '00000000' 
+001620         MOVE FUNCTION CURRENT-DATE TO WS-CURR-DATE-21 
+001630         MOVE WS-CURR-DATE-21(1:8) TO LS-DATE-OUT(1:8) 
+001640      ELSE 
+001650         MOVE LS-DATE-IN            TO LS-DATE-OUT(1:8) 
+001651      END-IF 
+001660                                                         
+001900                                                         
+002000      GOBACK. 
