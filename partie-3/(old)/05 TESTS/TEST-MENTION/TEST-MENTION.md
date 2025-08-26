f02d479acac38d5ae0a0523cae16413a06c60ac4
Sylvain Ni <sylvainni@msn.com>
2025-08-26 16:50:15 +0200

    version fonctionnelle partie 3

---
+++ partie-3/(old)/05 TESTS/TEST-MENTION/TEST-MENTION.md
@@ -0,0 +1,11 @@
+ASSEQ - OK, in testcont
+  JCASSEQ - OK, in testcont
+TP09 - OK, program to test
+  JCTP09 - OK, in TP09
+SMENTION - OK, subprog
+  JCMENTO - OK, in TP09
+TMENTION
+  JCTMENTO
+  JETMENTO
+
+pour le point 2, se référer à l'exercise sur le chiffre d'affaire où on fait sous forme de tableau
---
+++ partie-3/SQL/SELECT_EXTRACT.sql
@@ -0,0 +1,26 @@
+   SELECT 
+       C.COMPANY, 
+       C.ADDRESS, 
+       C.CITY, 
+       C.ZIP, 
+       C.STATE, 
+       O.O_NO, 
+       CHAR(O.O_DATE) AS ODATE_ISO, -- 'YYYY-MM-DD' 
+       D.DNAME, 
+       E.LNAME, 
+       E.FNAME, 
+       I.P_NO, 
+       P.DESCRIPTION, 
+       I.QUANTITY, 
+       I.PRICE, 
+       DECIMAL(I.QUANTITY * I.PRICE, 7, 2) AS LINE_TOTAL 
+   FROM 
+       API6.ORDERS O 
+       INNER JOIN API6.CUSTOMERS C ON O.C_NO = C.C_NO 
+       INNER JOIN API6.EMPLOYEES E ON O.S_NO = E.E_NO 
+       INNER JOIN API6.DEPTS     D ON E.DEPT = D.DEPT 
+       INNER JOIN API6.ITEMS     I ON O.O_NO = I.O_NO 
+       INNER JOIN API6.PRODUCTS  P ON I.P_NO = P.P_NO 
+   ORDER BY 
+       O.O_NO, I.P_NO; 
+                                                                    
